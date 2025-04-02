71078f130f9a9419f5ee6466f7534c6acd85ecd7
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-04-02 06:11:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/", // Ensure this matches your repo name
+  //base: "/", // Ensure this matches your repo name
 });
