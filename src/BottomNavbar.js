0a4846063234321ae6cec9d371ece87be0125350
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-04-03 23:53:38 +0530

    Update BottomNavbar.js

--- src/BottomNavbar.js
+++ src/BottomNavbar.js
@@ -1,11 +1,21 @@
 export default {
     template: `
-    <div>
-    <div class="bottom-navbar">
-        <router-link to="/">Home</router-link>
-        <router-link to="/more">More</router-link>
-        <router-link to="/settings">Settings</router-link>
-    </div>
-    </div>
-    `
+        <div class="bottom-nav d-flex justify-content-around py-2 bg-dark text-white">
+            <router-link to="/" class="nav-item">
+                <i class="fas fa-home"></i><br>Home
+            </router-link>
+            <router-link to="/wallet" class="nav-item">
+                <i class="fas fa-wallet"></i><br>Wallet
+            </router-link>
+            <router-link to="/buy" class="nav-item">
+                <i class="fas fa-shopping-cart"></i><br>Buy
+            </router-link>
+            <router-link to="/sell" class="nav-item">
+                <i class="fas fa-handshake"></i><br>Sell
+            </router-link>
+            <router-link to="/history" class="nav-item">
+                <i class="fas fa-history"></i><br>History
+            </router-link>
+        </div>
+    `,
 };
