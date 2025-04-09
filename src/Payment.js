50aac8c11034ac763638f367307da96d60e11e37
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-04-10 03:55:53 +0530

    Update Payment.js

--- src/Payment.js
+++ src/Payment.js
@@ -1,6 +1,22 @@
 export default {
-    template: `
+  inject: ['cart'],
+  computed: {
+    total() {
+      return this.cart.reduce((sum, p) => sum + p.price * p.qty, 0);
+    }
+  },
+  methods: {
+    pay() {
+      alert("Payment successful!");
+      this.cart.length = 0;
+      this.$router.push("/");
+    }
+  },
+  template: `
     <div>
+      <h2>Checkout</h2>
+      <p>Total: <strong>\${{ total }}</strong></p>
+      <button class="btn btn-success" @click="pay">Pay Now</button>
     </div>
-    `
+  `
 };
