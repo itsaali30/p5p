64be49febf6ec846094bf7b8b513e573252b99b6
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-04-10 20:43:38 +0530

    Update Shop.js

--- src/Shop.js
+++ src/Shop.js
@@ -1,18 +1,16 @@
-export default {
+ export default {
   template: `
     <div>
-      <h2 class="mb-3">Products</h2>
-      <input v-model="search" class="form-control mb-4" placeholder="Search products..." />
-
+      <h2>Products</h2>
+      <input v-model="search" class="form-control mb-3" placeholder="Search products..." />
       <div class="row">
-        <div class="col-md-4 mb-4" v-for="product in filtered" :key="product.id">
-          <div class="card h-100 shadow-sm">
+        <div class="col-md-4 mb-3" v-for="product in filtered" :key="product.id">
+          <div class="card h-100">
             <div class="card-body d-flex flex-column">
-              <h5 class="card-title">{{ product.name }}</h5>
-              <p class="card-text text-muted">\${{ product.price.toFixed(2) }}</p>
-              <router-link :to="'/item/' + product.id" class="btn btn-primary mt-auto">
-                <i class="fas fa-eye me-1"></i> View
-              </router-link>
+              <h5>{{ product.name }}</h5>
+              <h5>{{ product.desc }}</h5>
+              <p>\${{ product.price }}</p>
+              <router-link :to="'/item/' + product.id" class="btn btn-primary mt-auto">View</router-link>
             </div>
           </div>
         </div>
@@ -24,7 +22,7 @@ export default {
   },
   computed: {
     filtered() {
-      return this.$store.state.products.filter(p =>
+      return this.$store.state.products.filter(p => 
         p.name.toLowerCase().includes(this.search.toLowerCase())
       );
     }
